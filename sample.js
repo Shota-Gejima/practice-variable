a14669bb54841ed5c5636278048d7f8345a89654
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-12-14 02:58:11 +0000

    var

--- sample.js
+++ sample.js
@@ -1,10 +1,4 @@
-let orange = 120;
-let apple = 120;
-
-if(orange < apple){
-  alert("みかんよりりんごの値段が高いです");
-} else if (orange == apple){
-  alert("みかんとりんごの値段は同じです");
-} else{
-  alert("みかんの値段がりんごより高いです");
-}
+var nickname = "taro";
+console.log(nickname);
+var nickname = "ichiro";
+console.log(nickname);

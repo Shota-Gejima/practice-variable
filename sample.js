65657b3f09ebd4f46179670d2cc5957409e75b1f
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-12-14 02:36:04 +0000

    [if]

--- sample.js
+++ sample.js
@@ -1,4 +1,10 @@
-let nicname = "taro";
-console.log(nicname);
-nicname = "ichiro";
-console.log(nicname);
+let orange = 120;
+let apple = 120;
+
+if(orange < apple){
+  alert("みかんよりりんごの値段が高いです");
+} else if (orange == apple){
+  alert("みかんとりんごの値段は同じです");
+} else{
+  alert("みかんの値段がりんごより高いです");
+}
